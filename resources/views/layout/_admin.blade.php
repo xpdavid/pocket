92f1d37a65958672ae42d2fd4538e3ed6d877dc1
Xiao Pu <acesscp@gmail.com>
2016-05-25 11:58:06 +0800

    deployment configuration
    1. change to better post csrf token

--- resources/views/layout/_admin.blade.php
+++ resources/views/layout/_admin.blade.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="zh-CN">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf_token" content="{{ \Crypt::encrypt(csrf_token()) }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{!! config('pocket.siteName') !!} 管理页面</title>
 
     <!-- Bootstrap -->
